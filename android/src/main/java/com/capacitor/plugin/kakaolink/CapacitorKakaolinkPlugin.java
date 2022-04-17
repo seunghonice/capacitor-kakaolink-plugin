01761d116258af342b65529048acb1e636fe0e07
Seungho Hong <seungho.hong@kurlycorp.com>
2022-04-17 22:28:32 +0900

    fix: android 컴파일 에러 수정

--- android/src/main/java/com/capacitor/plugin/kakaolink/CapacitorKakaolinkPlugin.java
+++ android/src/main/java/com/capacitor/plugin/kakaolink/CapacitorKakaolinkPlugin.java
@@ -1,22 +1,22 @@
 package com.capacitor.plugin.kakaolink;
 
 import com.getcapacitor.JSObject;
+import com.getcapacitor.NativePlugin;
 import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
 import com.getcapacitor.PluginMethod;
-import com.getcapacitor.annotation.CapacitorPlugin;
 
-@CapacitorPlugin(name = "CapacitorKakaolink")
+@NativePlugin
 public class CapacitorKakaolinkPlugin extends Plugin {
 
-    private CapacitorKakaolink implementation = new CapacitorKakaolink();
+    //    private CapacitorKakaolink implementation = new CapacitorKakaolink();
 
     @PluginMethod
     public void echo(PluginCall call) {
         String value = call.getString("value");
 
         JSObject ret = new JSObject();
-        ret.put("value", implementation.echo(value));
+        //        ret.put("value", implementation.echo(value));
         call.resolve(ret);
     }
 }
